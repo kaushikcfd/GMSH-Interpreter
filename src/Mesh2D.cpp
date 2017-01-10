59220a3292ec4ee2d11609e16919c5682d5515ca
Kaushik Kulkarni <kaushikcfd@gmail.com>
2017-01-10 13:39:42 +0530

    Non-compiled code: Updated the `Mesh2d::readFromFile(string)` function.

--- src/Mesh2D.cpp
+++ src/Mesh2D.cpp
@@ -61,8 +61,7 @@ void Mesh2D::readFromFile(string filename) {
             memcpy(elements[i][2], nodes[node_index3], sizeof(Node*));
 
             for (int j=0; j<3; j++) {
-                --elementNodes[i][j];// This is because the numbering of the node should be chosen from 0 based indexing.
-                ++n_nodes_nbgElements[elementNodes[i][j]];// Increasing the nbg nodes for each
+                elements[i][j]->pushNbgElement(elements[i]);
             }
         }
 
@@ -70,51 +69,11 @@ void Mesh2D::readFromFile(string filename) {
             // Omitting the line, as other elements are of no use.
             getline(cin, s);
             --i;
-            --n_elements;
+            --noElements;
         }
     }
-
-    // Allocating memory for the nbg nodes array.
-    nodes_nbgElements = new int* [n_nodes];
-    nodes_nbgThetaStart = new float* [n_nodes];
-    nodes_nbgThetaEnd = new float* [n_nodes];
-    int *currentIndex = new int[n_nodes];
-    for(i=0; i< n_nodes; i++ ) {
-        nodes_nbgElements[i] = new int[n_nodes_nbgElements[i]];
-        nodes_nbgThetaStart[i] = new float [n_nodes_nbgElements[i]];
-        nodes_nbgThetaEnd[i] = new float [n_nodes_nbgElements[i]];
-        currentIndex[i] = 0;
-    }
-
-    for(i=0; i< n_elements; i++){
-        for(int j=0; j<3; j++) {
-            nodes_nbgElements[elementNodes[i][j]][currentIndex[elementNodes[i][j]]] = i;
-            ++currentIndex[elementNodes[i][j]];
-        }
-    }
-
-    // Updating the float **nodes_thetaStart; float ** nodes_thetaEnd;
-    for(i=0; i< n_nodes; i++) {
-        float x_current = nodes_x[i];
-        float y_current = nodes_y[i];
-        float x1, y1, x2, y2; // Co-ordinates of the other two noes in the given element.
-        for(int j =0; j < n_nodes_nbgElements[i]; j++) {
-            int nbgElement_index = nodes_nbgElements[i][j];
-            assigningOtherCoords((unsigned)i, (unsigned)nbgElement_index, x1, y1, x2, y2);
-
-            thetaStart = ANGLE(atan2(y1 - y_current, x1- x_current)*180.0/3.1415926);
-            thetaEnd = ANGLE(atan2(y2 - y_current, x2- x_current)*180.0/3.1415926);
-            if(!((((thetaEnd - thetaStart) > 0) && ((thetaEnd - thetaStart) < 180)) ||(((thetaEnd - thetaStart) > -360) && ((thetaEnd - thetaStart) < -180)) ))
-                swap(thetaEnd, thetaStart);
-
-            nodes_nbgThetaStart[i][j] = thetaStart;
-            nodes_nbgThetaEnd[i][j] = thetaEnd;
-        }
-    }
-
-
-    delete [] currentIndex;
     fclose(pFile);
+
     return;
 }
 
