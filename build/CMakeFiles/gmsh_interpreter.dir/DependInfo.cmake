4c5fdf48728551cd073b1683cb122003ea4de02d
Kaushik Kulkarni <kaushikcfd@gmail.com>
2017-03-02 23:58:44 +0530

    Added a new directory for Grids

--- build/CMakeFiles/gmsh_interpreter.dir/DependInfo.cmake
+++ build/CMakeFiles/gmsh_interpreter.dir/DependInfo.cmake
@@ -4,20 +4,20 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/src/Element.cpp" "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/Element.cpp.o"
-  "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/src/Mesh2D.cpp" "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/Mesh2D.cpp.o"
-  "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/src/Node.cpp" "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/Node.cpp.o"
-  "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/src/main.cpp" "/home/kaushikcfd/MyStuff/Dropbox/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/main.cpp.o"
+  "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/src/Element.cpp" "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/Element.cpp.o"
+  "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/src/Mesh2D.cpp" "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/Mesh2D.cpp.o"
+  "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/src/Node.cpp" "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/Node.cpp.o"
+  "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/src/main.cpp" "/home/kaushik/MyStuff/MyGit/GMSH-Interpreter/build/CMakeFiles/gmsh_interpreter.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- test/plotMesh.py
+++ test/plotMesh.py
@@ -10,14 +10,14 @@ y = []
 triangles = []
 
 
-with open('arbit_Nodes.dat', 'r') as f:
+with open('square-refined-Nodes.dat', 'r') as f:
 	noNodes = int(f.readline())
 	for i in range(noNodes):
 		x_y = map(float, f.readline().strip().split())
 		x.append(x_y[0])
 		y.append(x_y[1])
 
-with open('arbit_Elements.dat', 'r') as f:
+with open('square-refined-Elements.dat', 'r') as f:
 	noElements = int(f.readline())
 	for i in range(noElements):
 		node1_2_3 = map(int, f.readline().strip().split())
@@ -26,7 +26,7 @@ with open('arbit_Elements.dat', 'r') as f:
 plt.figure()
 plt.gca().set_aspect('equal')
 #plt.axis([-1.2, 1.2, -1.2, 1.2])
-plt.triplot(x, y, triangles, 'b-')
+plt.triplot(x, y, triangles, color='black', linewidth=0.1)
 plt.title('Triplot of user-specified triangulation')
 plt.xlabel('X')
 plt.ylabel('Y')
